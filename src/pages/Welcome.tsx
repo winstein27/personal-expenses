da761b9433922d97607d11ff462ba16d754aa70d
Winstein Martins <sriwinstein@gmail.com>
2023-03-13 22:39:16 -0300

    :lipstick: feat: welcome page
    Adding content and styling welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,11 +1,24 @@
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import theme from '../styles/theme';
+
+const Box = styled.div`
+  background: ${theme.colors.cardBackground};
+  padding: 2rem;
+  width: fit-content;
+  margin: auto;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: 2rem;
+`;
 
 const Welcome = () => {
   return (
-    <>
-      <h1>Welcome!</h1>
-      <Link to={'/expenses'}>Go to expenses</Link>
-    </>
+    <Box>
+      <Title>Welcome to Personal Expenses!</Title>
+    </Box>
   );
 };
 
